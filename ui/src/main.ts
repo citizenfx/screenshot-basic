851390928c22cf30f012f8b85a2b8e9be81229a3
jonas <jonassvensson4@hotmail.com>
2020-05-23 12:01:20 +0200

    CFX update: Black screenshot fix?
    Fetched the update which the CFX elements pushed to the original repo. According to the project lead this along side with some client updates should fix the black screenshot.

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -49,12 +49,17 @@ class ScreenshotUI {
     rtTexture: any;
     sceneRTT: any;
     cameraRTT: any;
+    material: any;
     request: ScreenshotRequest;
 
     initialize() {
         window.addEventListener('message', event => {
             this.request = event.data.request;
-        })
+        });
+
+        window.addEventListener('resize', event => {
+            this.resize();
+        });
 
         const cameraRTT: any = new OrthographicCamera( window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -10000, 10000 );
         cameraRTT.position.z = 100;
@@ -62,30 +67,30 @@ class ScreenshotUI {
         const sceneRTT: any = new Scene();
 
         const rtTexture = new WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: LinearFilter, magFilter: NearestFilter, format: RGBAFormat, type: UnsignedByteType } );
-        const gameTexture: any = new CfxTexture( );
+        const gameTexture: any = new CfxTexture();
         gameTexture.needsUpdate = true;
-
-        const material = new ShaderMaterial( {
-
+   
+        const material = new ShaderMaterial({
             uniforms: { "tDiffuse": { value: gameTexture } },
             vertexShader: `
-			varying vec2 vUv;
+                varying vec2 vUv;
 
-			void main() {
-				vUv = vec2(uv.x, 1.0-uv.y); // fuck gl uv coords
-				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-			}
-`,
+                void main() {
+                    vUv = vec2(uv.x, 1.0-uv.y); // fuck gl uv coords
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+                }
+            `,
             fragmentShader: `
-			varying vec2 vUv;
-			uniform sampler2D tDiffuse;
+                varying vec2 vUv;
+                uniform sampler2D tDiffuse;
 
-			void main() {
-				gl_FragColor = texture2D( tDiffuse, vUv );
-			}
-`
+                void main() {
+                    gl_FragColor = texture2D( tDiffuse, vUv );
+                }
+            `
+        });
 
-        } );
+        this.material = material;
 
         const plane = new PlaneBufferGeometry( window.innerWidth, window.innerHeight );
         const quad: any = new Mesh( plane, material );
@@ -108,6 +113,26 @@ class ScreenshotUI {
         this.animate = this.animate.bind(this);
 
         requestAnimationFrame(this.animate);
+    }
+
+    resize() {
+        const cameraRTT: any = new OrthographicCamera( window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -10000, 10000 );
+        cameraRTT.position.z = 100;
+
+        this.cameraRTT = cameraRTT;
+
+        const sceneRTT: any = new Scene();
+
+        const plane = new PlaneBufferGeometry( window.innerWidth, window.innerHeight );
+        const quad: any = new Mesh( plane, this.material );
+        quad.position.z = -100;
+        sceneRTT.add( quad );
+
+        this.sceneRTT = sceneRTT;
+
+        this.rtTexture = new WebGLRenderTarget( window.innerWidth, window.innerHeight, { minFilter: LinearFilter, magFilter: NearestFilter, format: RGBAFormat, type: UnsignedByteType } );
+
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
     }
 
     animate() {
@@ -137,7 +162,6 @@ class ScreenshotUI {
 
         // draw the image on the canvas
         const d = new Uint8ClampedArray(read.buffer);
-
         const cxt = canvas.getContext('2d');
         cxt.putImageData(new ImageData(d, window.innerWidth, window.innerHeight), 0, 0);
 
@@ -199,6 +223,7 @@ class ScreenshotUI {
         fetch(request.targetURL, {
             method: 'POST',
             mode: 'cors',
+            cache: 'no-store',
             headers: request.headers,
             body: body
         })
@@ -208,6 +233,7 @@ class ScreenshotUI {
                 fetch(request.resultURL, {
                     method: 'POST',
                     mode: 'cors',
+                    cache: 'no-store',
                     body: JSON.stringify({
                         data: text,
                         id: request.correlation
